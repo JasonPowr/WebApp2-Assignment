9775b09fceed782e4fa34a2a327b906417d7bc98
Jason Power <20076537@mail.wit.ie>
2021-11-24 17:25:50 +0000

    Added Api link for top rated

--- src/api/tmdb-api.js
+++ src/api/tmdb-api.js
@@ -16,7 +16,15 @@ export const getMovies = () => {
 
 export const getNowPlaying = () => {
   return fetch(
-    `https://api.themoviedb.org/3/movie/now_playing?api_key=${process.env.REACT_APP_TMDB_KEY}&language=en-US&page=1`
+    `https://api.themoviedb.org/3/movie/now_playing?api_key=${process.env.REACT_APP_TMDB_KEY}&language=en-US&page=1`  //extra
+  )
+    .then(res => res.json())
+    .then(json => json.results);
+};
+
+export const getTopRated = () => {
+  return fetch(
+    `https://api.themoviedb.org/3/movie/top_rated?api_key=${process.env.REACT_APP_TMDB_KEY}&language=en-US&page=1`  //extra
   )
     .then(res => res.json())
     .then(json => json.results);
