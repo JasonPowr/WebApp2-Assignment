8451b6386d40e9702404c08b83bb38ab1de8fdd9
Jason Power <20076537@mail.wit.ie>
2021-12-08 14:48:24 +0000

    Functioning authentication using Auth0 and protected routes need to add styling.

--- src/index.js
+++ src/index.js
@@ -14,10 +14,10 @@ import AddMovieReviewPage from './pages/addMovieReviewPage';
 import AddNowPlayingMoviePage from './pages/nowPlayingPage';
 import AddTopRatedPage from './pages/topRated';
 import AddMostPopularPage from './pages/popularPage';
-import { Auth0Provider } from "@auth0/auth0-react";
-import LoginButton from "./components/LoginButton/LoginButton";
-import LogoutButton from "./components/LogoutButton/LogoutButton";
+import { Auth0Provider, useAuth0 } from "@auth0/auth0-react";
 import AddLoginPage from "./pages/LoginPage";
+import PrivateRoute from "./protectedRoute";
+
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -33,7 +33,9 @@ const domain = process.env.REACT_APP_AUTH0_DOMAIN;
 const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID;
 
 
+
 const App = () => {
+
   return (
     <QueryClientProvider client={queryClient}>
       <BrowserRouter>
@@ -41,16 +43,16 @@ const App = () => {
         <MoviesContextProvider>
             {" "}
             <Switch>
-            <Route exact path="/" component={AddLoginPage} />
-            <Route exact path="/movies/popular" component={AddMostPopularPage} />
-            <Route exact path="/movies/topRated" component={AddTopRatedPage} />
-            <Route exact path="/movies/nowplaying" component={AddNowPlayingMoviePage} />
-            <Route exact path="/reviews/form" component={AddMovieReviewPage} />
-            <Route path="/reviews/:id" component={MovieReviewPage} />
-            <Route exact path="/movies/upcoming" component={UpcomingMoviesPage} />
-            <Route exact path="/movies/favorites" component={FavoriteMoviesPage} />
-            <Route path="/movies/:id" component={MoviePage} />
-            <Route exact path="/" component={HomePage} />
+            <Route exact path="/login" component={AddLoginPage} />         
+            <PrivateRoute path="/movies/popular" component={AddMostPopularPage} />
+            <PrivateRoute path="/movies/topRated" component={AddTopRatedPage} />
+            <PrivateRoute path="/movies/nowplaying" component={AddNowPlayingMoviePage} />
+            <PrivateRoute path="/reviews/form" component={AddMovieReviewPage} />
+            <PrivateRoute path="/reviews/:id" component={MovieReviewPage} />
+            <PrivateRoute path="/movies/upcoming" component={UpcomingMoviesPage} />
+            <PrivateRoute path="/movies/favorites" component={FavoriteMoviesPage} />
+            <PrivateRoute path="/movies/:id" component={MoviePage} />
+            <PrivateRoute exact path="/" component={HomePage} />
             <Redirect from="*" to="/" />
         </Switch>
         </MoviesContextProvider>
--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,20 +1,21 @@
-import React, { useState, useEffect } from "react";
-import PageTemplate from '../components/templateMovieListPage';
-import { Auth0Provider } from "@auth0/auth0-react";
-import LoginButton from '../components/LoginButton/LoginButton';
-import LogoutButton from '../components/LogoutButton/LogoutButton'
+import React, { useContext, useState } from "react"
+import { Route, Redirect } from "react-router-dom";
+import { useAuth0 } from "@auth0/auth0-react";
+import LoginButton from "../components/Buttons/LoginButton";
+import LogoutButton from "../components/Buttons/Logout Button";
 
 const LoginPage = (props) => {
+const { isAuthenticated } = useAuth0();
 
 return (
     <>
-    <p>It works</p>
-    <LoginButton />
-    <LogoutButton />
+        <LoginButton />
+        <LogoutButton />
+        <h3> User is {isAuthenticated ? "Logged in" : "Not Logged in"}</h3>
     </>
-
-)
+);
 };
 
 export default LoginPage;
+
 
